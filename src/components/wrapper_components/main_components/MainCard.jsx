ca06c2817882a336db3fc300ddbfcc4c52dcb57d
ulukmyrza2001 <ulukmyrzazhanybekov01@gmail.com>
2022-01-13 20:40:48 +0600

    ui коштум

--- src/components/wrapper_components/main_components/MainCard.jsx
+++ src/components/wrapper_components/main_components/MainCard.jsx
@@ -1,14 +1,16 @@
+import Card from "../ui/Card.jsx"
+
 function MainCard(props) {
     return(
         props.data.map(el=>{
             return(
-                <div className='food-card'>
+                <Card key = {el.id}>
                     <img className='food-img' src={el.img} alt='' />
                     <div>
                         <div>{el.title}</div>
                         <div>{el.price}</div>
                     </div>
-                </div>
+                </Card>
             )
         })
     )
